351cdb9d95d63539e97487dbba94b5777c37c5c9
TianleZhang <cqtianle@163.com>
2018-01-06 20:50:48 +0800

    添加 重要指标是否在正常范围内特征 删除 性别特征、年龄分组特征

--- tl/DataAnalysis.py
+++ tl/DataAnalysis.py
@@ -1,7 +1,7 @@
 from sys import path
 path.append('../')
 import pandas as pd
-from tl.util import read_data, save, plt_encoding_error, error
+from tl.util import read_data, save, plt_encoding_error, error, normal
 import matplotlib.pyplot as plt
 
 train, test_A, _ = read_data()
@@ -11,17 +11,23 @@ num_test_A = len(test_A)
 
 train_m = pd.concat([train, test_A])
 
-train_y = train['血糖']
-train_m.drop(['id', '血糖', '体检日期'], axis=1, inplace=True)
+train_m['甘油三酯_normal'] = train_m['甘油三酯'].apply(normal, args=(1.69,))
+train_m['尿素_normal'] = train_m['尿素'].apply(normal, args=(7.1,))
+train_m['尿酸_normal'] = train_m.apply(lambda x: 0 if (x['性别']=='女' and x['尿酸']>357) or (x['性别']=='男' and x['尿酸']>416) else 1, axis=1)
+train_m['*天门冬氨酸氨基转换酶_normal'] = train_m['*天门冬氨酸氨基转换酶'].apply(normal, args=(40,))
 
 # 年龄分组
-age_cut = pd.cut(train_m['年龄'], [1, 20, 30, 35, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70, 73, 76, 79, 82, 85, 88])
-age_cut = pd.DataFrame({'age': age_cut})
-# age_cut.rename(columns={'年龄': 'age'}, inplace=True)
-train_m = pd.concat([train_m, age_cut], axis=1)
-train_m.drop(['年龄'], axis=1, inplace=True)
+# age_cut = pd.cut(train_m['年龄'], [1, 20, 30, 35, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70, 73, 76, 79, 82, 85, 88])
+# age_cut = pd.DataFrame({'age': age_cut})
+# # age_cut.rename(columns={'年龄': 'age'}, inplace=True)
+# train_m = pd.concat([train_m, age_cut], axis=1)
+# # train_m.drop(['年龄'], axis=1, inplace=True)
 
-train_m = pd.get_dummies(train_m, columns=['性别', 'age'])
+# train_m = pd.get_dummies(train_m, columns=['性别','age'])
+
+drop_list = ['id', '血糖','体检日期','性别']
+train_y = train['血糖']
+train_m.drop(drop_list, axis=1, inplace=True)
 
 # 重新切分训练与测试数据
 train_x = train_m.iloc[:num_train]
@@ -53,7 +59,7 @@ params = {
     'num_leaves': 31,
     'learning_rate': 0.02,
     'lambda_l1': 1,
-    'lambda_l2': 1,
+    # 'lambda_l2': 1,
     'cat_smooth': 10,
     'feature_fraction': 0.8,
     'bagging_freq': 5,
@@ -76,17 +82,17 @@ error(y_test, y_pred)
 
 # online
 
-# gbm_online = lgb.train(params,
-#                        train_all,
-#                        num_boost_round=106)
-# # predict
-# predict = gbm_online.predict(test_X, num_iteration=gbm_online.best_iteration)
-# data1 = pd.DataFrame(predict)
-# # save
-# save(data1, 'lgb')
+gbm_online = lgb.train(params,
+                       train_all,
+                       num_boost_round=235)
+# predict
+predict = gbm_online.predict(test_X, num_iteration=gbm_online.best_iteration)
+data1 = pd.DataFrame(predict)
+# save
+save(data1, 'lgb')
 
 # 解决中文乱码
 plt_encoding_error()
 
-# lgb.plot_importance(gbm_online)
-# plt.show()
+lgb.plot_importance(gbm)
+plt.show()
--- tl/util.py
+++ tl/util.py
@@ -5,17 +5,34 @@ import configparser
 import pandas as pd
 import datetime
 from sklearn.metrics import mean_squared_error
+import math
 
 
 def plt_encoding_error():
     # coding:utf-8
     import matplotlib
-    matplotlib.use('qt4agg')
+    # matplotlib.use('qt4agg')
     # 指定默认字体
     matplotlib.rcParams['font.sans-serif'] = ['SimHei']
     matplotlib.rcParams['font.family'] = 'sans-serif'
     # 解决负号'-'显示为方块的问题
     matplotlib.rcParams['axes.unicode_minus'] = False
+
+
+def normal(feature, value, height=1):
+    """
+    Determine whether the value is normal or missing
+    :param feature:
+    :param value: feature's value
+    :param height: 1 means over the value is abnormal, 0 means under the value is abnormal
+    :return: if normal return 0, abnormal return 1, missing return -1
+    """
+    if math.isnan(feature):
+        return -1
+    elif (height == 1 and feature > value) or (height == 0 and feature < value):
+        return 1
+    else:
+        return 0
 
 
 def get_url():
@@ -31,9 +48,9 @@ def get_url():
 def read_data():
     root_dir, train_url, feature_url = get_url()
 
-    train = pd.read_csv(root_dir + 'd_train_20180102.csv')
-    test_A = pd.read_csv(root_dir + 'd_test_A_20180102.csv')
-    sample = pd.read_csv(root_dir + 'd_sample_20180102.csv')
+    train = pd.read_csv(root_dir + 'd_train_20180102.csv', encoding='gb2312')
+    test_A = pd.read_csv(root_dir + 'd_test_A_20180102.csv', encoding='gb2312')
+    sample = pd.read_csv(root_dir + 'd_sample_20180102.csv', encoding='gb2312')
     return train, test_A, sample
 
 
