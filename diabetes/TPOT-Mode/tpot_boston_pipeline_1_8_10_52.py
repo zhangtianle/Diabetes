01c445d1ba99ba7280ef168a0af1478105159a92
altmanWang <qx_wang@foxmail.com>
2018-01-08 14:48:46 +0800

    利用TPOP提分

---
+++ diabetes/TPOT-Mode/tpot_boston_pipeline_1_8_10_52.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.linear_model import RidgeCV
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.svm import LinearSVR
+from tpot.builtins import StackingEstimator
+
+# NOTE: Make sure that the class is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'].values, random_state=42)
+
+# Score on the training set was:-1.966247144875928
+exported_pipeline = make_pipeline(
+    MaxAbsScaler(),
+    StackingEstimator(estimator=LinearSVR(C=0.01, dual=False, epsilon=1.0, loss="squared_epsilon_insensitive", tol=0.001)),
+    StackingEstimator(estimator=RidgeCV()),
+    ExtraTreesRegressor(bootstrap=False, max_features=0.7000000000000001, min_samples_leaf=4, min_samples_split=6, n_estimators=100)
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
